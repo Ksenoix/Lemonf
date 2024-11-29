d816b53100c5b34687ffb0ee9d977e97547ce04f
Ksenoix <kantorns2010@gmail.com>
2024-11-29 16:44:04 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="background-color-zadnica-gitler">11</div>
+      <ul>
+        <li>fguft</li>
+      </ul>
+    </div>
+  );
+}
